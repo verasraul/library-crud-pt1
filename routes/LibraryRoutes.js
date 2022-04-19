50d74e3915caf634a58fd16c603c18ec186ac088
Raul Veras <verasraul@gmail.com>
2022-04-19 19:51:30 -0400

    fixing typos

--- routes/LibraryRoutes.js
+++ routes/LibraryRoutes.js
@@ -1,4 +1,4 @@
-const express = requre('express');
+const express = require('express');
 const app = express();
 const bookModel = require('../models/book');
 
@@ -6,12 +6,11 @@ const bookModel = require('../models/book');
 app.get('/books', async (req, res) => {
     const books = await bookModel.find({})
 
-
     try{
         response.send(books)
     } catch (error){
         response.status(500).send(error)
     }
-})
+});
 
-module.exports = app
+module.exports = app;
--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const cors = require('cors')
 const libraryRouter = require('./routes/LibraryRoutes.js')
 
 const app = express()
-app.use(express().json)
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 
 PORT = process.env.PORT;
@@ -20,7 +20,7 @@ mongoose.connect(MONGO_URI, {
     useUnifiedTopology: true,
 });
 
-app.listen(PORT, ( => {
+app.listen(PORT, () => {
     console.log('Server is running on port:', PORT)
-}))
+})
 
